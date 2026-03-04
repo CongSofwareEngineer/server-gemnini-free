b4c362325f48ab62e54214a8980aecb32e3f79f6
congbacoor <cong@bacoor.co>
2026-03-04 16:28:40 +0700

    fix api key

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const port = 3000
+require('dotenv').config()
 
-app.use(cors())
+const app = express()
+const port = process.env.PORT || 3005
 app.use(express.json())
+app.use(cors())
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,21 +16,30 @@ app.post(`/gemini/:model`, async (req, res) => {
   try {
     const { model } = req.params
     const body = req.body
+    console.log({ model, body });
+
     const resApi = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'x-goog-api-key': 'AIzaSyCvl6hpysHOkitFSYdXMYk7Ph56679SYbA',
+        'x-goog-api-key': process.env.API_KEY_GEMINI,
+
       },
       body: JSON.stringify(body)
     })
     const data = await resApi.json()
-    res.send(data)
+
+    if (data?.error?.code) {
+      res.status(data?.error?.code).send(data)
+    } else {
+      res.send(data)
+    }
+
   } catch (error) {
     res.status(500).send(error)
   }
 })
 
-app.listen(process.env.PORT || 3005, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
